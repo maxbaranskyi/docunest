fe7293603953f9f1356396c398d112ffe4378034
Max Baranskyi <max.baranskyi@gmail.com>
2025-09-17 21:43:42 +0300

    install vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import TestComponent from './components/TestComponent.vue';
+
+const app = createApp({});
+
+app.component('test-component', TestComponent);
+
+app.mount('#app');
